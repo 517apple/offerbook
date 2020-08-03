dabb8e9ef58688fb8dc5247493be3243e87c435a
apple <1833221215@qq.com>
2020-08-03 18:44:17 +0800

    2020-8-4 17:22:57

---
+++ offerbook/二叉树的创建和便利/二叉树的创建和遍历.cpp
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream> 
+
+
+/*
+二叉树的一些常规操作：
+1. 创建
+2. 遍历（前中后序遍历）
+
+参考链接1：https://blog.csdn.net/qq_40693171/article/details/99745321
+*/
+using namespace std;
+
+struct BinaryTreeNode
+{
+	int m_nValue = 0;
+	BinaryTreeNode* m_pLeft = NULL;
+	BinaryTreeNode* m_pRight = NULL;
+};
+
+//建树算法
+BinaryTreeNode* createBiTree() {
+	BinaryTreeNode* root;
+	int ch;
+	cin >> ch;
+	if (ch == 0)
+		root = NULL;
+	else {
+		root = new BinaryTreeNode;
+		root->m_nValue = (int)ch;
+		root->m_pLeft = createBiTree();
+		root->m_pRight = createBiTree();
+	}
+	return root;
+}
+
+/*
+二叉树的前序遍历
+*/
+void qianxu(BinaryTreeNode* node)
+{
+	if (node != NULL)
+	{
+		printf("%d\t", node->m_nValue);
+		qianxu(node->m_pLeft);
+		qianxu(node->m_pRight);
+	}
+
+}
+
+/*
+二叉树的中序遍历
+*/
+void zhongxu(BinaryTreeNode* node)
+{
+	if (node != NULL)
+	{
+		zhongxu(node->m_pLeft);
+		//cout << node->m_nValue << "  ";
+		printf("%d\t", node->m_nValue);
+		zhongxu(node->m_pRight);
+	}
+
+}
+
+/*
+二叉树的后序遍历
+*/
+void houxu(BinaryTreeNode* node)
+{
+	if (node != NULL)
+	{
+		houxu(node->m_pLeft);
+		houxu(node->m_pRight);
+		printf("%d\t", node->m_nValue);
+	}
+
+}
+
+int main(void)
+{
+	BinaryTreeNode* root = createBiTree();
+
+	cout << "前序遍历：";
+	qianxu(root);
+	cout << endl;
+
+	cout << "中序遍历：";
+	zhongxu(root);
+	cout << endl;
+
+	cout << "后序遍历：";
+	houxu(root);
+	cout << endl;
+
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+
+}
+
--- offerbook/复述/复述.cpp
+++ offerbook/复述/复述.cpp
@@ -1,80 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream> 
-
+//二叉树的建立算法
+#include <iostream>
 using namespace std;
-
-/*
-不借助新的空间实现链表的反转
-*/
-
-struct ListNode
-{
-	int val=0;
-	ListNode* next=NULL;
-};
-
-
-ListNode* rev(ListNode* head)
-{
-	if (head == NULL)
-		return NULL;
-
-	ListNode* NewHead = NULL;
-	ListNode* CurNode = head;
-	ListNode* OldHead = head;
-	while (OldHead!=NULL)
-	{
-		CurNode = OldHead;
-		OldHead = OldHead->next;
-		CurNode->next = NewHead;
-		NewHead = CurNode;
+typedef struct node {
+	int data;
+	struct node* lchild;
+	struct node* rchild;
+}Node, * BTnode;
+//建树算法
+Node* createBiTree() {
+	BTnode root;
+	int ch;
+	cin >> ch;
+	if (ch == 0)
+		root = NULL;
+	else {
+		root = new node;
+		root->data = ch;
+		root->lchild = createBiTree();
+		root->rchild = createBiTree();
 	}
-	return NewHead;
+	return root;
 }
-
-void PrintNL(ListNode* head)
-{
-	if (head == NULL)
-		return;
-
-	while (head!=NULL)
-	{
-		printf("%d\t", head->val);
-		head = head->next;
+void preorder(struct node* root) {
+	if (root) {
+		cout << root->data << " ";
+		preorder(root->lchild);
+		preorder(root->rchild);
 	}
-	printf("\n");
 }
-
-
-
-int main(void)
-{
-	ListNode a;
-	ListNode b;
-	ListNode c;
-	ListNode d;
-	ListNode e;
-
-	a.val = 1;
-	a.next = &b;
-	b.val = 2;
-	b.next = &c;
-	c.val = 3;
-	c.next = &d;
-	d.val = 4;
-	d.next = &e;
-	e.val = 5;
-	e.next = NULL;
-
-	PrintNL(&a);
-
-	ListNode* node;
-	node = rev(&a);
-	PrintNL(node);
-
-	cout << "hello world" << endl;
+int main() {
+	BTnode T;
+	T = NULL;
+	T = createBiTree();
+	cout << "建树成功" << endl;
+	preorder(T);
 	system("pause");
-	return 0;
 
 }
-
