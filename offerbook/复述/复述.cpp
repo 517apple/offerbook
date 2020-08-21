a39b8ac52cfe2ada7d2f6ca41836bb4f5eeb3a14
apple <1833221215@qq.com>
2020-08-21 22:59:34 +0800

    fu复习二叉树

--- offerbook/复述/复述.cpp
+++ offerbook/复述/复述.cpp
@@ -1,12 +1,69 @@
 #define _CRT_SECURE_NO_WARNINGS
 //二叉树的建立算法
 #include <iostream>
+#include <vector>
+#include <deque>
 using namespace std;
 typedef struct node {
 	int data;
 	struct node* lchild;
 	struct node* rchild;
 }Node, * BTnode;
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	
+};
+
+
+class Solution {
+public:
+	int minDepth(TreeNode* root) {
+		if (root == NULL)
+			return 0;
+		int deeplen = 0;
+		deque<TreeNode*> detree;
+		detree.push_back(root);
+		vector<int> v1;
+		while (detree.size())
+		{
+			TreeNode* node = detree.front();
+			v1.push_back(node->val);
+			if (node->left != NULL)
+				detree.push_back(node->left);
+			else
+				v1.push_back(node->val);
+
+			if (node->right != NULL)
+				detree.push_back(node->right);
+			else
+				v1.push_back(node->val);
+
+			if ((node->right == NULL) && (node->left == NULL))
+				return
+
+		}
+
+	}
+};
+
+
+
 //建树算法
 Node* createBiTree() {
 	BTnode root;
