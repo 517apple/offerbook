a5d252dade24e5f43eef2aa2c3426cdee26161fb
apple <1833221215@qq.com>
2020-09-06 23:11:38 +0800

    2020-9-6 23:11:28

---
+++ offerbook/lt107二叉树的层次遍历/二叉树的层次遍历.cpp
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream> 
+#include <vector>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+/*
+给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+我的思路是使用两个队列，一个存储当前节点，另一个队列存储下一个序列的节点
+
+*/
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	
+};
+
+
+vector<vector<int>> levelOrderBottom(TreeNode* root) {
+	
+
+	deque<TreeNode*> curdenode, nextdenode;
+	vector<vector<int>> tarvec;
+	stack<vector<int>> stackvec;
+	
+	if(root!=NULL)
+		curdenode.push_back(root);
+
+	while (curdenode.size())
+	{
+		vector<int> vec;
+		while (curdenode.size())
+		{
+			vec.push_back(curdenode.front()->val);
+			if (curdenode.front()->left != NULL)
+				nextdenode.push_back(curdenode.front()->left);
+			if (curdenode.front()->right != NULL)
+				nextdenode.push_back(curdenode.front()->right);
+			curdenode.pop_front();
+		}
+		stackvec.push(vec);
+		//tarvec.push_back(vec);
+		curdenode = nextdenode;
+		nextdenode.clear();
+	}
+
+	while (stackvec.size())
+	{
+		tarvec.push_back(stackvec.top());
+		stackvec.pop();
+	}
+
+	return tarvec;
+
+}
+
+
+int main(void)
+{
+
+
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+
+}
+
--- offerbook/复述/复述.cpp
+++ offerbook/复述/复述.cpp
@@ -1,134 +1,116 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream> 
-#include <deque>
 #include <vector>
 #include <string>
+#include <deque>
 
 using namespace std;
 
-/*
-复习二叉树的一些常用操作，包括建树、前中后序遍历、层序遍历
-同时加深对递归迭代的理解
-*/
 
-struct BinTreeNode
+
+struct BintreeNode
 {
 	int val = 0;
-	BinTreeNode* pLeft = NULL;
-	BinTreeNode* pRight = NULL;
+	BintreeNode* pLeft = NULL;
+	BintreeNode* pRight = NULL;
 };
 
-/*
-建树算法：
-1. 使用键盘输入动态建树
-2. 使用数组快速建树进行验证
-*/
 
-BinTreeNode* CreateTree()
+/*
+建树
+*/
+BintreeNode* CreateTree()
 {
-	BinTreeNode* root=new BinTreeNode;
+	BintreeNode* node = new BintreeNode;
 	int ch;
-	cin>>ch;
+	cin >> ch;
 	if (ch == 0)
-		root = NULL;
+		node = NULL;
 	else
 	{
-		root->val = (int)ch;
-		root->pLeft = CreateTree();
-		root->pRight = CreateTree();
+		
+		node->val = (int)ch;
+		node->pLeft = CreateTree();
+		node->pRight=CreateTree();
 	}
-	return root;
-}
 
-/*
-前序遍历
-*/
-void qianxu(BinTreeNode* root) 
-{
-	if (root == NULL)
-		return;
-	cout << root->val<<" ";
-	qianxu(root->pLeft);
-	qianxu(root->pRight);
+	return node;
 }
 
 /*
 层序遍历打印树
 */
-void printTree(BinTreeNode* root)
+void printTree(BintreeNode* root)
 {
-	deque<BinTreeNode*> detree;
 	if (root == NULL)
 		return;
-	else
-		detree.push_back(root);
-
+	cout << "--------------遍历树-------------" << endl;
+	deque<BintreeNode*> detree;
+	detree.push_back(root);
 	while (detree.size())
 	{
-		BinTreeNode* CurNode;
-		CurNode = detree.front();
-		cout << CurNode->val << " ";
-		if (CurNode->pLeft != NULL)
-			detree.push_back(CurNode->pLeft);
-		if (CurNode->pRight != NULL)
-			detree.push_back(CurNode->pRight);
+		cout << detree.front()->val << " ";
+		if (detree.front()->pLeft != NULL)
+			detree.push_back(root->pLeft);
+		if (detree.front()->pRight != NULL)
+			detree.push_back(root->pRight);
 
 		detree.pop_front();
 	}
 	cout << endl;
+
+}
+
+/*
+前序遍历
+*/
+void qianxu(BintreeNode* root)
+{
+	if (root == NULL)
+		return;
+	cout << root->val << " ";
+	qianxu(root->pLeft);
+	qianxu(root->pRight);
 }
 
 
 /*
 二叉树的所有路径
 */
-void construct_paths(BinTreeNode* root, string path, vector<string>& paths)
+void construct_path(BintreeNode* root, string path, vector<string>& paths)
 {
 	if (root == NULL)
 		return;
-
 	path += to_string(root->val);
-	if (root->pLeft == NULL && root->pRight == NULL)
+	if (root->pLeft==NULL && root->pRight==NULL)
 	{
 		paths.push_back(path);
 	}
 	else
 	{
 		path += "->";
-		construct_paths(root->pLeft, path, paths);
-		construct_paths(root->pRight, path, paths);
+		construct_path(root->pLeft, path, paths);
+		construct_path(root->pRight, path, paths);
 	}
 
 }
 
-
 int main(void)
 {
-	BinTreeNode* root = CreateTree();
-	printTree(root);
-	qianxu(root);
-	cout << endl;
+	BintreeNode* root = CreateTree();
 
-	cout << "--------------分割线-------------" << endl;
-	vector<string> paths;
+	/*qianxu(root);
+	printTree(root);*/
+
 	string path = "";
-	construct_paths(root, path, paths);
-	
-	//迭代器遍历方式
-#if 0
+	vector<string> paths;
+
+	construct_path(root, path, paths);
+
 	for (vector<string>::iterator iter=paths.begin();iter!=paths.end();iter++)
 	{
 		cout << *iter << endl;
 	}
-#else
-	//通过数组下标遍历
-	for (size_t i = 0; i < paths.size(); i++)
-	{
-		cout << paths.at(i) << endl;
-	}
-
-#endif
-
 
 	cout << "hello world" << endl;
 	system("pause");
