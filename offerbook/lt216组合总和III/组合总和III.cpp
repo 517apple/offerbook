fafe7d819e246ceb56c02ffc88bbd9cf53394077
apple <1833221215@qq.com>
2020-09-11 12:07:06 +0800

    2020-9-11 12:07:02

---
+++ offerbook/lt216组合总和III/组合总和III.cpp
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream> 
+#include <vector>
+
+
+using namespace std;
+
+/*
+找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+https://leetcode-cn.com/problems/combination-sum-iii/
+
+QAQ：使用vector时访问越界的原因还没有找到
+*/
+
+vector<int> nums = { 1,2,3,4,5,6,7,8,9 };
+
+vector<vector<int>> res;
+vector<int> temp;
+
+void dfs(int index,int k, int target)
+{
+	if (target == 0 && k==0)
+	{
+		res.push_back(temp);
+		return;
+	}
+	if (target-index < 0 || k==0)
+		return;
+
+	for (int i = index; i <= 9; i++)
+	{
+		//剪枝
+		if (target - i < 0)
+			break;
+		temp.push_back(i);
+		dfs(i + 1, k - 1, target - i);
+		temp.pop_back();
+	}
+}
+
+
+void dfs1(int index, int k, int target)
+{
+	if (target == 0 && k == 0)
+	{
+		res.push_back(temp);
+		return;
+	}
+	if (target - nums[index] < 0 || k == 0)
+		return;
+
+	for (int i = index; i < 9; i++)
+	{
+		//剪枝
+		if (target - nums[i] < 0)
+			break;
+
+		temp.push_back(nums[i]);
+
+		dfs1(i + 1, k - 1, target - nums[i]);
+
+		temp.pop_back();
+	}
+}
+
+
+
+vector<vector<int>> combinationSum3(int k, int n) {
+	dfs1(0, k, n);
+	return res;
+}
+
+
+int main(void)
+{
+	vector<vector<int>> ans;
+	ans = combinationSum3(3, 9);
+
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+
+}
+
